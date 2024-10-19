d86c7325b28914f0b62817494f4ecc56ed0ff979
M.Wajahat Ullah Durrani <96327689+WAJAHATDURRANI1@users.noreply.github.com>
2024-10-19 17:15:33 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var btn = document.createElement("btn");
+  var txt = document.createTextNode("\u00D7");
+  btn.className = "close";
+  btn.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
